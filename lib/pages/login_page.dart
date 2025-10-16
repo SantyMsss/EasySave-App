08517875666d59545cdb31321be142b96147dcdf
Santiago Martinez <santiagomarse@hotmail.com>
2025-10-15 23:00:02 -0500

    base code

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:easysave/pages/widgets/sign_in.dart';
+import 'package:easysave/pages/widgets/sign_up.dart';
+import 'package:easysave/theme.dart';
+import 'package:easysave/utils/bubble_indicator_painter.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  late PageController _pageController;
+
+  Color left = Colors.black;
+  Color right = Colors.white;
+
+  @override
+  void dispose() {
+    _pageController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      physics: const ClampingScrollPhysics(),
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[
+                  CustomTheme.primaryGreen,
+                  CustomTheme.primaryPurple
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 1.0),
+                stops: <double>[0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 75.0),
+                child: Column(
+                  children: [
+                    Image(
+                        height:
+                            MediaQuery.of(context).size.height > 800 ? 80.0 : 60,
+                        fit: BoxFit.contain,
+                        image: const AssetImage('assets/img/login_logo.png')),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'EasySave',
+                      style: TextStyle(
+                        color: CustomTheme.white,
+                        fontSize: 28,
+                        fontFamily: 'WorkSansBold',
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    const Text(
+                      'Programate a Ahorrar',
+                      style: TextStyle(
+                        color: CustomTheme.white,
+                        fontSize: 14,
+                        fontFamily: 'WorkSansMedium',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: _buildMenuBar(context),
+              ),
+              Expanded(
+                flex: 2,
+                child: PageView(
+                  controller: _pageController,
+                  physics: const ClampingScrollPhysics(),
+                  onPageChanged: (int i) {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if (i == 0) {
+                      setState(() {
+                        right = Colors.white;
+                        left = Colors.black;
+                      });
+                    } else if (i == 1) {
+                      setState(() {
+                        right = Colors.black;
+                        left = Colors.white;
+                      });
+                    }
+                  },
+                  children: <Widget>[
+                    ConstrainedBox(
+                      constraints: const BoxConstraints.expand(),
+                      child: const SignIn(),
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints.expand(),
+                      child: const SignUp(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget _buildMenuBar(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 50.0,
+      decoration: const BoxDecoration(
+        color: Color(0x55FFFFFF),
+        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+      ),
+      child: CustomPaint(
+        painter: BubbleIndicatorPainter(pageController: _pageController),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+                onPressed: _onSignInButtonPress,
+                child: Text(
+                  'Iniciar Sesión',
+                  style: TextStyle(
+                      color: left,
+                      fontSize: 16.0,
+                      fontFamily: 'WorkSansSemiBold'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+                onPressed: _onSignUpButtonPress,
+                child: Text(
+                  'Registrarse',
+                  style: TextStyle(
+                      color: right,
+                      fontSize: 16.0,
+                      fontFamily: 'WorkSansSemiBold'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSignInButtonPress() {
+    _pageController.animateToPage(0,
+        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  void _onSignUpButtonPress() {
+    _pageController.animateToPage(1,
+        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+}
